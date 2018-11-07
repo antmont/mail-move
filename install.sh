abc5d23cfebd7aef62d665de26e5b415cd7a950e
Anthony M <antmontalt@me.com>
2018-11-07 20:51:06 +1100

    Create install script

--- install.sh
+++ install.sh
@@ -2,5 +2,13 @@
 
 # Install mail move automation components
 
-cp com.fusiforms.mailmove.plist $HOME/Library/LaunchAgents/com.fusiforms.mailmove.plist
-launchctl load $HOME/Library/LaunchAgents/com.fusiforms.mailmove.plist
+# Directory for temporarily storing files
+mkdir -p ~/Downloads/mail
+
+# Install Applescript where it can be accessed by mail
+cp -f mail-save-attachment-v2.scpt ~/Library/Application\ Scripts/com.apple.mail/mail-save-attachment-v2.scpt
+
+# Copy plist to the correct location for user agents and load it
+launchctl unload ~/Library/LaunchAgents/com.fusiforms.mailmove.plist
+cp com.fusiforms.mailmove.plist ~/Library/LaunchAgents/com.fusiforms.mailmove.plist
+launchctl load ~/Library/LaunchAgents/com.fusiforms.mailmove.plist
